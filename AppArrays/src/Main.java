c52b5ba6cdaa914ec825feb9671cd712ae2e8e57
LeoR1409 <rleo47225@gmail.com>
2024-05-11 09:35:36 -0500

    user

--- AppArrays/src/Main.java
+++ AppArrays/src/Main.java
@@ -21,8 +21,7 @@ public class Main {
 
 
         //Hacer doble bucle for/for
-
-
+        String user = config[XPROD][CUSER];
         //CLAVE: DESARROLLO
 
         //System.out.println(config[3][1]);
